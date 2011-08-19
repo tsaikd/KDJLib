4025f87ed0e7e8698c82c59a09c3e00bea4d4b37
tsaikd <cheng-feng.tsai@foxconn.com>
2011-08-19 11:32:32 +0800

    add ProcessStdoutThread

---
+++ src/org/tsaikd/java/utils/ProcessStdoutThread.java
@@ -0,0 +1,57 @@
+package org.tsaikd.java.utils;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.log4j.Logger;
+
+public class ProcessStdoutThread extends Thread {
+	Logger log = Logger.getLogger(ProcessStdoutThread.class);
+	Process prog = null;
+	Type type = Type.Stdout;
+
+	public enum Type {
+		Stdout,
+		Stderr,
+	}
+
+	public ProcessStdoutThread(Process prog) {
+		this.prog = prog;
+	}
+
+	public ProcessStdoutThread(Process prog, Type type) {
+		this.prog = prog;
+		this.type = type;
+	}
+
+	public void run() {
+		String line;
+		try {
+			InputStream is;
+			BufferedReader brCleanUp;
+
+			switch (type) {
+			case Stderr:
+				is = prog.getErrorStream();
+				brCleanUp = new BufferedReader(new InputStreamReader(is));
+				while ((line = brCleanUp.readLine ()) != null) {
+					log.error(line);
+				}
+				break;
+			case Stdout:
+			default:
+				is = prog.getInputStream();
+				brCleanUp = new BufferedReader(new InputStreamReader(is));
+				while ((line = brCleanUp.readLine ()) != null) {
+					log.info(line);
+				}
+				break;
+			}
+
+			brCleanUp.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
