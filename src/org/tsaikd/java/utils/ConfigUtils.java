6c32af171963e50ad40d9af7acbc773798445897
tsaikd <cheng-feng.tsai@foxconn.com>
2011-09-23 16:55:14 +0800

    ConfigUtils support multiple properties

--- src/org/tsaikd/java/utils/ConfigUtils.java
+++ src/org/tsaikd/java/utils/ConfigUtils.java
@@ -1,8 +1,12 @@
 package org.tsaikd.java.utils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 
 import org.apache.commons.logging.Log;
@@ -12,25 +16,47 @@ public class ConfigUtils {
 
 	static Log log = LogFactory.getLog(ConfigUtils.class);
 
-	protected static final String defaultResPath = "/config.properties";
-	protected static Map<String, Properties> confs = new HashMap<String, Properties>();
+	static final String defaultResPath = "/config.properties";
+	static ConfigUtils instance = new ConfigUtils();
+
+	public class PropInfo {
+		Properties prop = null;
+		String path = null;
+		long modTime = 0;
+		boolean autoReaload = false;
+	}
+
+	List<PropInfo> propList = new LinkedList<PropInfo>();
+	Collection<String> pathList = new HashSet<String>();
 
 	protected ConfigUtils() {
-	}
-
-	public static Properties getInstance(String resPath) throws Exception {
-		Properties property = confs.get(resPath);
-		if (property == null) {
-			property = new Properties();
-			InputStream in = Thread.currentThread().getClass().getResourceAsStream(resPath);
-			property.load(in);
-			in.close();
+		try {
+			addClassResource(defaultResPath);
+		} catch (Exception e) {
 		}
-		return property;
 	}
 
-	public static Properties getInstance() throws Exception {
-		return getInstance(defaultResPath);
+	public static ConfigUtils addProperties(PropInfo info) {
+		if (info == null) {
+			return instance;
+		}
+		instance.propList.add(0, info);
+		instance.pathList.add(info.path);
+		return instance;
+	}
+
+	public static ConfigUtils addClassResource(String path, boolean autoReload) throws Exception {
+		if (instance.pathList.contains(path)) {
+			return instance;
+		}
+
+		PropInfo info = getPropInfoByPath(path);
+		info.autoReaload = autoReload;
+		return addProperties(info);
+	}
+
+	public static ConfigUtils addClassResource(String path) throws Exception {
+		return addClassResource(path, false);
 	}
 
 	public static void set(final String key, final String value) throws Exception {
@@ -42,13 +68,38 @@ public class ConfigUtils {
 			return null;
 		}
 		String value = System.getProperty(key);
-		if (value == null) {
-			value = getInstance().getProperty(key);
-			if (value == null) {
-				value = defaultValue;
+		if (value != null) {
+			return value;
+		}
+
+		for (PropInfo info : instance.propList) {
+			if (info.autoReaload && (info.path != null)) {
+				File file = new File(info.path);
+				if (file.exists() && file.canRead()) {
+					if ((file.lastModified() != info.modTime) || (info.prop == null)) {
+						if (info.prop == null) {
+							info.prop = new Properties();
+						} else {
+							info.prop.clear();
+						}
+						info.prop.load(new FileInputStream(file));
+						info.modTime = file.lastModified();
+						log.debug("Reload file: " + info.path);
+					}
+				}
+			}
+
+			if (info.prop == null) {
+				continue;
+			}
+
+			value = info.prop.getProperty(key);
+			if (value != null) {
+				return value;
 			}
 		}
-		return value;
+
+		return defaultValue;
 	}
 
 	public static String get(final String key) throws Exception {
@@ -56,7 +107,7 @@ public class ConfigUtils {
 	}
 
 	public static void set(final String key, final int value) throws Exception {
-		getInstance().setProperty(key, String.valueOf(value));
+		System.setProperty(key, String.valueOf(value));
 	}
 
 	public static int getInt(final String key, final int defaultValue) throws Exception {
@@ -73,7 +124,7 @@ public class ConfigUtils {
 	}
 
 	public static void set(final String key, final long value) throws Exception {
-		getInstance().setProperty(key, String.valueOf(value));
+		System.setProperty(key, String.valueOf(value));
 	}
 
 	public static long getLong(final String key, final long defaultValue) throws Exception {
@@ -90,7 +141,7 @@ public class ConfigUtils {
 	}
 
 	public static void set(final String key, final boolean value) throws Exception {
-		getInstance().setProperty(key, String.valueOf(value));
+		System.setProperty(key, String.valueOf(value));
 	}
 
 	public static boolean getBool(final String key, final boolean defaultValue) throws Exception {
@@ -109,6 +160,33 @@ public class ConfigUtils {
 
 	public static boolean getBool(final String key) throws Exception {
 		return getBool(key, false);
+	}
+
+	public static PropInfo getPropInfoByPath(String path) throws Exception {
+		PropInfo info = instance.new PropInfo();
+		InputStream is;
+
+		info.path = path;
+
+		is = Thread.currentThread().getClass().getResourceAsStream(path);
+
+		if (is == null) {
+			File file = new File(path);
+			if (file.exists() && file.canRead()) {
+				is = new FileInputStream(file);
+				info.modTime = file.lastModified();
+			}
+		}
+
+		if (is != null) {
+			info.prop = new Properties();
+			info.prop.load(is);
+			is.close();
+		} else {
+			log.debug("Can not load file: " + path);
+		}
+
+		return info;
 	}
 
 }
